b892ad73b6df20f81a2ea011e88e5c3bc0688ff3
aradosz79 <artur@icole.eu>
2019-08-26 02:43:23 +0200

    auto configuration

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM dockette/jdk8:latest
 
-RUN apk add --no-cache wget bash; \
+RUN apk add --no-cache wget bash gettext; \
  wget -O /tmp/promtail.gz https://github.com/grafana/loki/releases/download/v0.3.0/promtail_linux_amd64.gz; \
  gzip -d /tmp/promtail.gz; cp /tmp/promtail /usr/local/bin/promtail; chmod 755 /usr/local/bin/promtail; rm /tmp/*;\
   mkdir /etc/promtail; mkdir /app; mkdir /app/log
--- docker-entry.sh
+++ docker-entry.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
-/usr/local/bin/promtail -config.file=/etc/promtail/promtail.yml&
+if test -z "$DOCKER_NODE"; then export DOCKER_NODE="UNKNOWN"; fi
+if test -z "$DOCKER_APP"; then export DOCKER_APP="UNKNOWN"; fi
+if test -z "$LOKI"; then export LOKI="http://172.18.111.33:3100"; fi
+
+envsubst </etc/promtail/promtail.yml >/etc/promtail/promtail-env.yml
+
+/usr/local/bin/promtail -config.file=/etc/promtail/promtail-env.yml&
 exec "$@"
